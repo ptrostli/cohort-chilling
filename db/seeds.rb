c320ca435395fb29603c50de1c87277614cb8977
Philip Trostli <philiptrostli@gmail.com>
2022-11-17 11:24:37 -0500

    tables, models, controllers, serializers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+event_1 = Event.find_or_create_by(
+  title: "Anna's Birthday!",
+  date: 'November 23, 2022',
+  time: '5:45PM',
+  city: 'Boston',
+  state: 'MA',
+  zip: '01234',
+  address: '1 Real Street',
+  event_type: 'Chilling',
+  description: 'Get drunk.'
+)
+
+event_2 = Event.find_or_create_by(
+  title: 'LA Friday Funtime',
+  date: 'November 30, 2022',
+  time: '2:45PM',
+  city: 'Boston',
+  state: 'MA',
+  zip: '01234',
+  address: '1 Real Street',
+  event_type: 'Chilling',
+  description: 'Playing with the squad'
+)
+
+event_3 = Event.find_or_create_by(
+  title: 'Candlepin Bowling',
+  date: 'November 25, 2022',
+  time: '7:00PM',
+  city: 'Cambridge',
+  state: 'MA',
+  zip: '55555',
+  address: '1 Homer Simpson Ave',
+  event_type: 'Chilling',
+  description: 'Drinks, bowling, good times.'
+)
+
+user_1 = User.create(
+  email: "email@email.com",
+  password: "password",
+)
+
+user_2 = User.create(
+  email: "phil@email.com",
+  password: "password",
+)
